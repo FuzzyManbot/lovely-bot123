5c11c23568baae7d4c68f6a3ba4fe7a653bf28a8
FuzzyManbot <37940170+FuzzyManbot@users.noreply.github.com>
2018-03-30 22:23:33 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,9 +11,9 @@ client.on('message', message => {
   	}
 });
 
-.on('message', message => {
+client.on('message', message => {
     if (message.content === 'announcement-for-30.03.2018-22-20') {
-    	message.reply('Excuse me! Fuzzy will be taking me to bed for some time. I will be offline during this period! Either Fuzzy is just fixing some bugs or either hes adding some super super super cool stuff! to me!');
+    	message.reply(' Told me to say this: Fuzzy will be taking me to bed for some time. I will be offline during this period! Either Fuzzy is just fixing some bugs or either hes adding some super super super cool stuff! to me!');
   	}
 });
 
