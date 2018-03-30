d3a38cb3d0456f37569473e136f12b81e969f3c2
FuzzyManbot <37940170+FuzzyManbot@users.noreply.github.com>
2018-03-30 21:53:26 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,24 +5,11 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
-const prefix = "-";
-client.on(`message`, (msg) => {
-    if (msg.author.equals(client.user)) return;
-    if (msg.channel.type == "dm") return;
-    if (!msg.content.startsWith(prefix)) return;
-
-    var message = msg.content.toLowerCase();
-    var args = message.substring(prefix.length).split(" ");
-
-if (args[0].endsWith == "announce") {
-    var lD = prefix + args[0] + " ";
-        var annouce= msg.content.substr(lD.length);
-
-    let embed = new Discord.MessageEmbed()
-        .addField('Annoucment', annouce)
-        .setColor("GOLD");
-msg.channel.send(embed);
-}
+client.on('message', message => {
+    if (message.content === 'ping') {
+    	message.reply('pong');
+  	}
 });
 
+// THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
